2c835ab68569e1b972f34ba480eeb9f474454775
Tomer Figenblat <tomer.figenblat@gmail.com>
2021-02-10 14:48:56 +0200

    style: added license headers

--- src/test/java/info/tomfi/alexa/skillstester/steps/impl/Assertion_Method_notWaitForFollowup_Test.java
+++ src/test/java/info/tomfi/alexa/skillstester/steps/impl/Assertion_Method_notWaitForFollowup_Test.java
@@ -1,3 +1,15 @@
+/**
+ * Copyright Tomer Figenblat.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package info.tomfi.alexa.skillstester.steps.impl;
 
 import static org.assertj.core.api.BDDAssertions.then;
@@ -25,16 +37,14 @@ final class Assertion_Method_notWaitForFollowup_Test {
   @InjectMocks ThenResponseImpl sut;
 
   @Test
-  void asserting_with_a_closed_session_will_keep_ongoing_assertion(
-      @Mock final Response response) {
+  void asserting_with_a_closed_session_will_keep_ongoing_assertion(@Mock final Response response) {
     given(response.getShouldEndSession()).willReturn(true);
     given(responseEnvelope.getResponse()).willReturn(response);
     then(sut.notWaitForFollowup()).isEqualTo(sut);
   }
 
   @Test
-  void asserting_with_an_open_session_will_throw_an_assertion_error(
-      @Mock final Response response) {
+  void asserting_with_an_open_session_will_throw_an_assertion_error(@Mock final Response response) {
     given(response.getShouldEndSession()).willReturn(false);
     given(responseEnvelope.getResponse()).willReturn(response);
     thenExceptionOfType(AssertionError.class)
