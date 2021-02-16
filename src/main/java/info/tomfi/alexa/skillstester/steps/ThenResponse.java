88735688f6db7da26997f6fc8f7cf1e0b5c4975f
Tomer Figenblat <tomer.figenblat@gmail.com>
2021-02-16 14:41:20 +0200

    feat: added haveNoOutputSpeech assertion method

--- src/main/java/info/tomfi/alexa/skillstester/steps/ThenResponse.java
+++ src/main/java/info/tomfi/alexa/skillstester/steps/ThenResponse.java
@@ -146,6 +146,13 @@ public abstract class ThenResponse {
   public abstract ThenResponse waitForFollowup();
 
   /**
+   * Assert the response has no output speech object.
+   *
+   * @return this instance for further assertions.
+   */
+  public abstract ThenResponse haveNoOutputSpeech();
+
+  /**
    * Assert the response has no reprompt object.
    *
    * @return this instance for further assertions.
